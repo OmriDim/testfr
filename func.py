31ff042a947501f8196574314ce9ff78780d1547
omri <omri@localhost.localdomain>
2020-11-09 05:09:57 -0800

    first

---
+++ func.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+def func1(a, b):
+    c=a+b
+    return c
+def test_answer():
+    assert func1(2,2) == 4
+test_answer()
+print(func1(int(sys.argv[1]),int(sys.argv[2])))
+
+
+
